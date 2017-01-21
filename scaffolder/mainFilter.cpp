8042fd44595d963727e86d2208de626a6b5c5d33
Olga Chernikova <Oche24912@yandex.ru>
2017-01-21 18:51:50 +0300

    execute dif files

---
+++ scaffolder/mainFilter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "GraphControl.h"
+
+using namespace std;
+
+#include "ContigGraph/InteractiveFilter.h"
+
+int main(int argc, char **argv) {
+    InteractiveFilter::main();
+    return 0;
+}
