ee2a44393f86ea73a4cf088c29df855c4f56071d
Olga Chernikova <Oche24912@yandex.ru>
2017-07-05 16:03:14 +0300

    simple fileter test adaper edges and libs

--- scaffolder/tests/FiltersTest/filterTest.cpp
+++ scaffolder/tests/FiltersTest/filterTest.cpp
@@ -11,13 +11,23 @@ protected:
 
     void SetUp() {
         graphFileName = "/tmp/somefile.gr";
-        logging::create_console_logger("../log.properties");
+        logging::create_console_logger("../../../log.properties");
     }
 
     std::string createInfoByCoord(const int* coord) {
         std::stringstream res;
         res << "\ncoord: " << coord[0] << "-" << coord[1] << "\n" << coord[2] << "-" << coord[3];
         return res.str();
+    }
+
+    std::vector<std::vector<int > > gen_gr(const int * from, int cnt_e, int cnt_v) {
+        std::vector<std::vector<int> > g(cnt_v);
+
+        for (int i = 0; i < cnt_e; ++i) {
+            g[from[i]].push_back(i);
+        }
+
+        return g;
     }
 };
 
@@ -92,6 +102,62 @@ TEST_F(FiltersTest, testAdapterVertex) {
         curName << "node" << i;
         ASSERT_EQ(curName.str(), adapter.getTargetName(i));
     }
+}
+
+TEST_F(FiltersTest, testAdapterEdges) {
+    graphFileName = "../../../resources/filterTest/simple_adapter_graph.gr";
+    const int LIB_CNT = 2;
+    const int VERT_CNT = 8;
+    const int EDGE_CNT = 10;
+    const int VERT_TO[EDGE_CNT] = {2, 2, 6, 4, 2, 1, 1, 3, 3, 5};
+    const int VERT_FROM[EDGE_CNT] = {0, 0, 2, 2, 4, 3, 3, 7, 5, 3};
+    const int EDGES_WEIGHT[EDGE_CNT] = {4, 1, 1, 2, 1, 4, 1, 1, 2, 1};
+    const int EDGES_LIB[EDGE_CNT] = {0, 1, 1, 0, 1, 0, 1, 1, 0, 1};
+
+    std::vector<std::vector<int> > gr = gen_gr(VERT_FROM, EDGE_CNT, VERT_CNT);
+    std::vector<std::vector<int> > grR =  gen_gr(VERT_TO, EDGE_CNT, VERT_CNT);
+
+    contig_graph::ContigGraph graph;
+    filter::FilterAdapter adapter(graph);
+
+    adapter.processQuery(filter::Query(filter::Query::UPLOAD_GRAPH, graphFileName));
+
+    std::vector<int> vert = adapter.getVertexList();
+    for (int v : vert) {
+        TRACE("v = " << v);
+        std::vector<int> edges = adapter.getEdges(v);
+        ASSERT_EQ(gr[v], edges);
+        std::vector<int> edgesR = adapter.getEdgesR(v);
+        ASSERT_EQ(grR[v], edgesR);
+    }
+
+    for (int e = 0; e < EDGE_CNT; ++e) {
+        ASSERT_EQ(VERT_TO[e], adapter.getEdgeTo(e));
+        ASSERT_EQ(VERT_FROM[e], adapter.getEdgeFrom(e));
+        ASSERT_EQ(EDGES_WEIGHT[e], adapter.getEdgeWeight(e));
+        ASSERT_EQ(EDGES_LIB[e], adapter.getEdgeLib(e));
+    }
+}
+
+TEST_F(FiltersTest, testAdapterLibs) {
+    graphFileName = "../../../resources/filterTest/simple_adapter_graph.gr";
+    const int LIB_CNT = 2;
+    contig_graph::ContigGraph graph;
+    filter::FilterAdapter adapter(graph);
+
+    adapter.processQuery(filter::Query(filter::Query::UPLOAD_GRAPH, graphFileName));
+
+    ASSERT_EQ(LIB_CNT, adapter.getLibList().size());
+    std::vector<int> libs = adapter.getLibList();
+    for (int i = 0; i < LIB_CNT; ++i) {
+        ASSERT_EQ(i, libs[i]);
+        ASSERT_EQ(adapter.getLibType(i), contig_graph::ContigGraph::Lib::RNA_PAIR);
+    }
+
+    ASSERT_EQ("#ff0000", adapter.getLibColor(0));
+    ASSERT_EQ("#00ff00", adapter.getLibColor(1));
+    ASSERT_EQ("lib1", adapter.getLibName(0));
+    ASSERT_EQ("lib2", adapter.getLibName(1));
 }
 
 /*TEST_F(FiltersTest, testMinWeight) {
