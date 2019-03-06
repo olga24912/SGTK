b5979acf3bd8f5a889e0b31796db3debffa3208a
Olga Chernikova <Oche24912@yandex.ru>
2019-03-06 09:54:34 +0300

    create simple test

--- UI/test/simple_test.py
+++ UI/test/simple_test.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get('file:///home/olga/CAB/SGTK_/SGTK/UI/mainPage.html')
-driver.quit()
+class Selenium2OnLocal(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_from_local(self):
+        self.driver.get('file:///home/olga/CAB/SGTK_/SGTK/UI/mainPage.html')
+        self.assertEqual('SGTK', self.driver.title)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
