412675c9b3b3b7158255031e7f0150fc6e87902b
Olga Chernikova <Oche24912@yandex.ru>
2019-03-06 10:48:09 +0300

    create ignore data

--- UI/test/release_test.py
+++ UI/test/release_test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import unittest
+import os
 from selenium import webdriver
 
 class Selenium2OnLocal(unittest.TestCase):
     def setUp(self):
+        os.system("PREFIX=~/tmp/testSGTK  ../../compile.sh")
         self.driver = webdriver.Firefox()
 
     def test_from_local(self):
